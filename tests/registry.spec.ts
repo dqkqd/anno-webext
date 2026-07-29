f83987da53cbbf4da13259bf09a70b092865d4cd
Khanh Duong <dqkqdlot@gmail.com>
2026-07-30 07:31:14 +0900

    fix(spatial): consider scroll positions in query (#49)
    Closes #48

--- tests/registry.spec.ts
+++ tests/registry.spec.ts
@@ -145,3 +145,38 @@ test('query annotations on page load', async ({ annotatedUrls, context }) => {
   await page.getByText('three').first().hover();
   await expect(page.locator('#hover')).toHaveText('three');
 });
+
+test('query correct annotation after scrolling', async ({ annotatedUrls, context }) => {
+  const page = await context.newPage();
+  const urls = await annotatedUrls(`
+  <html>
+    <body>
+      <p style="height: 500px;">spacer</p>
+      <span>target text</span>
+      <p style="height: 1000px;">spacer</p>
+    </body>
+  </html>
+  `);
+  await page.goto(urls[0]);
+
+  await annotateText(page, (document) => {
+    const span = document.querySelector('span')!;
+    const range = document.createRange();
+    range.setStart(span.firstChild!, 0);
+    range.setEnd(span.firstChild!, 11);
+    return range;
+  });
+
+  // Scroll down a bit
+  await page.evaluate(() => window.scrollTo(0, 300));
+
+  // Make sure the target text in the view port
+  await expect(page.getByText('target text').first()).toBeInViewport();
+  // Hover it
+  await page.getByText('target text').first().hover();
+  await expect(page.locator('#hover')).toHaveText('target text');
+
+  // Move mouse out => hide
+  await page.mouse.move(0, 0);
+  await expect(page.locator('#hover')).toHaveText('');
+});
